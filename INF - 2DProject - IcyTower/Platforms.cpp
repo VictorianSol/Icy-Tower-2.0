e874868a330eaea3d26b79877331f1f0c36295fd
Victorian <victoriansolz@gmail.com>
2020-12-30 19:29:36 +0100

    add iron platformsTexture

--- INF - 2DProject - IcyTower/Platforms.cpp
+++ INF - 2DProject - IcyTower/Platforms.cpp
@@ -40,6 +40,7 @@ Platforms::Platforms(CameraView& view){
 	platformsTexture[0].loadFromFile("resources\\platforms.png");
 	platformsTexture[1].loadFromFile("resources\\platforms_ice.png");
 	platformsTexture[2].loadFromFile("resources\\platforms_wood.png");
+	platformsTexture[3].loadFromFile("resources\\platforms_iron.png");
 	platformsTextureSize = platformsTexture[0].getSize();
 	platformsTextureSize.x /= 20;
 	platformsTextureSize.y /= 16;
@@ -62,12 +63,16 @@ Platforms::Platforms(CameraView& view){
 
 void Platforms::draw(RenderWindow& window, CameraView& view) {
 	for (int i = 0; i < PLATFORMS_NUMBER; i++)
-		if ((platformLevel[i] / 100) % PLATFORMS_TYPE == 1) {
+		if ((platformLevel[i] / PLATFORMS_CHANGE) % PLATFORMS_TYPE == 1) {
 			platforms[i].setTexture(&platformsTexture[1]);
 			platforms[i].setFillColor(Color(220, 220, 224));
 		}
-		else if ((platformLevel[i] / 100) % PLATFORMS_TYPE == 2) {
+		else if ((platformLevel[i] / PLATFORMS_CHANGE) % PLATFORMS_TYPE == 2) {
 			platforms[i].setTexture(&platformsTexture[2]);
+			platforms[i].setFillColor(Color::White);
+		}
+		else if ((platformLevel[i] / PLATFORMS_CHANGE) % PLATFORMS_TYPE == 3) {
+			platforms[i].setTexture(&platformsTexture[3]);
 			platforms[i].setFillColor(Color::White);
 		}
 		else {
